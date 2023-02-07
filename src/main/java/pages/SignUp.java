ff877149dbc03c64ea8ed7538b60ef6d79f1fbfb
GhadeerMoh <ghadeer.seleem@gmail.com>
2023-02-07 15:11:59 +0200

    add random data using time stamp

--- src/main/java/pages/SignUp.java
+++ src/main/java/pages/SignUp.java
@@ -4,19 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.PageFactory;
 
 public class SignUp extends BaseClass {
-	
+
 	By loginPage = By.xpath("//a[contains(text(),' Signup / Login')]");
-	
-	
-	
+	By name = By.name("name");
+	By email = By.name("email");
+	By signUpButton = By.xpath("//button[contains(text(),'Signup')]");
+	By headerInformation = By.xpath("//*[contains(text(),'Enter Account Information')]");
+
 	public SignUp() {
 		PageFactory.initElements(driver, this);
 
 	}
-	
-	public void openLoginAndSignUpPage(){
-		
+
+	public void openLoginAndSignUpPage() {
 		driver.findElement(loginPage).click();
+	}
+	
+	public By signUpNewUser(String nameVal , String mailVal) {
+		driver.findElement(name).sendKeys(nameVal);
+		driver.findElement(email).sendKeys(mailVal);
+		driver.findElement(signUpButton).click();
+		return headerInformation;
 	}
 
 }
--- src/test/java/pageTests/SignUpTest.java
+++ src/test/java/pageTests/SignUpTest.java
@@ -1,27 +1,36 @@
 package pageTests;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import pages.BaseClass;
 import pages.SignUp;
 
-public class SignUpTest extends BaseClass{
-	
-	
+public class SignUpTest extends BaseClass {
+
+	SoftAssert softAssert = new SoftAssert();
+
 	@BeforeClass
 	public void init() {
 		setup();
 	}
-	
+
 	@Test
 	public void test() {
-		 new SignUp().openLoginAndSignUpPage();
+
+		String currentTime = String.valueOf(System.currentTimeMillis());
+		String email = "test" + currentTime + "@test.com";
+		new SignUp().openLoginAndSignUpPage();
+		By element = new SignUp().signUpNewUser("test", email);
+		boolean check = driver.findElement(element).isDisplayed();
+		softAssert.assertTrue(check);
 	}
 
 	@AfterTest
-	public void tearDown(){
+	public void tearDown() {
 		quitBrowser();
 	}
 
